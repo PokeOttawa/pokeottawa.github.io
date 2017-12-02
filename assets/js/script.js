d6383fa2fc067b1ffc4429cc59ce15f9c4fd98eb
Marina <avalai@live.com>
2017-12-01 23:31:42 -0500

    Reverting to lazy code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-document.onload() {
-
   document.getElementById("exp-file").onchange = function () {
       var reader = new FileReader();
 
       reader.onload = function (e) {
           // get loaded data and render thumbnail.
+          console.log(this.id);
           document.getElementById("exp-img").src = e.target.result;
       };
 
@@ -12,4 +11,28 @@ document.onload() {
       reader.readAsDataURL(this.files[0]);
   };
 
-}
+  document.getElementById("collector-file").onchange = function () {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          // get loaded data and render thumbnail.
+          console.log(this.id);
+          document.getElementById("collector-img").src = e.target.result;
+      };
+
+      // read the image file as a data URL.
+      reader.readAsDataURL(this.files[0]);
+  };
+
+  document.getElementById("jogger-file").onchange = function () {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          // get loaded data and render thumbnail.
+          console.log(this.id);
+          document.getElementById("jogger-img").src = e.target.result;
+      };
+
+      // read the image file as a data URL.
+      reader.readAsDataURL(this.files[0]);
+  };

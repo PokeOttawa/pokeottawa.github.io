b0d0b901c3aa618781b8dd17fdb33898443b6fda
Marina <avalai@live.com>
2017-12-02 00:04:40 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,12 @@
+/* Show preview image when user selects file for upload */
 ['exp', 'collector', 'jogger'].forEach(function(stat) {
   document.getElementById(stat + '-file').onchange = function () {
     var reader = new FileReader()
 
     reader.onload = function (e) {
-      // get loaded data and render thumbnail.
-      console.log(this.id)
       document.getElementById(stat + '-img').src = e.target.result
     }
 
-    // read the image file as a data URL.
     reader.readAsDataURL(this.files[0])
   }
 })

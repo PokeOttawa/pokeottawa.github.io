9a7e6ac42aed30d7f50fac14dfe049215770d55c
Marina <avalai@live.com>
2017-12-01 23:59:31 -0500

    Refactor image code to be brief

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,13 @@
-  document.getElementById("exp-file").onchange = function () {
-      var reader = new FileReader();
+['exp', 'collector', 'jogger'].forEach(function(stat) {
+  document.getElementById(stat + '-file').onchange = function () {
+    var reader = new FileReader()
 
-      reader.onload = function (e) {
-          // get loaded data and render thumbnail.
-          console.log(this.id);
-          document.getElementById("exp-img").src = e.target.result;
-      };
+    reader.onload = function (e) {
+      // get loaded data and render thumbnail.
+      console.log(this.id)
+      document.getElementById(stat + '-img').src = e.target.result
+    }
 
-      // read the image file as a data URL.
-      reader.readAsDataURL(this.files[0]);
-  };
-
-  document.getElementById("collector-file").onchange = function () {
-      var reader = new FileReader();
-
-      reader.onload = function (e) {
-          // get loaded data and render thumbnail.
-          console.log(this.id);
-          document.getElementById("collector-img").src = e.target.result;
-      };
-
-      // read the image file as a data URL.
-      reader.readAsDataURL(this.files[0]);
-  };
-
-  document.getElementById("jogger-file").onchange = function () {
-      var reader = new FileReader();
-
-      reader.onload = function (e) {
-          // get loaded data and render thumbnail.
-          console.log(this.id);
-          document.getElementById("jogger-img").src = e.target.result;
-      };
-
-      // read the image file as a data URL.
-      reader.readAsDataURL(this.files[0]);
-  };
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0])
+})
